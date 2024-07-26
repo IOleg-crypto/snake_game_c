08461b518774f432a498c996fca13aec78ee2ca8
IOleg-sus <guchkooleg7@gmail.com>
2024-07-26 14:59:00 +0300

    added keyboard support

--- main.c
+++ main.c
@@ -8,18 +8,55 @@
 #include <windows.h>
 #include <process.h>
 #include <curses.h>
+//constants
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
+typedef struct
+{
+     int x , y;
+} Vector2D;
 
 int main(int argc , char **argv)
 {
-    int screen_width = 80;
-    int screen_height = 25;
-
-    initscr();
+    //initialise screen
+    WINDOW *win = initscr();
+    
+    //take player input
+    keypad(win, true);
+    nodelay(win, true);
     curs_set(0);
-    noecho();
-    cbreak();
-    srand(time(NULL));
     
+    //snake parts
+    Vector2D head = {0 , 0};
+    Vector2D dir = {1 , 0};
     
-    return 0;
+    //game loop
+    while (true)
+    {
+        //pressed key
+        int pressed = wgetch(win);
+        switch (pressed){
+            case KEY_UP:
+              dir.x = -1;
+              dir.y = 0;
+              break;
+            case KEY_DOWN:
+              dir.x = 1;
+              dir.y = 0;
+              break;
+            case KEY_LEFT:
+              dir.x = 0;
+              dir.y = -1;
+              break;
+            case KEY_RIGHT:
+              dir.x = 0;
+              dir.y = 1;
+              break;
+            if (pressed == '\e')
+            {
+                break;
+            }
+        }
+    }
 }
